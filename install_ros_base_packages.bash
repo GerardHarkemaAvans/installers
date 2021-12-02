c4c5ca0247ff26b736b2a85c449add246cac6ee2
Gerard Harkema <ga.harkema@avans.nl>
2021-12-02 15:48:33 +0100

    20211202

--- install_ros_base_packages.bash
+++ install_ros_base_packages.bash
@@ -3,6 +3,7 @@
 sudo apt update
 sudo apt-get install python3-catkin-tools
 sudo apt-get install ros-melodic-ros-controllers ros-melodic-gazebo* ros-melodic-moveit* ros-melodic-industrial-core
+sudo apt install ros-melodic-rqt-joint-trajectory-controller
 sudo apt-get install ros-melodic-rgbd-launch ros-melodic-libuvc-camera
 sudo apt-get install ros-melodic-usb-cam
 sudo apt-get install ros-melodic-rosserial-arduino
